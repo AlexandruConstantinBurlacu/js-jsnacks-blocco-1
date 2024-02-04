05125c91d66fbd51b6abc6fd653566c1ff2e3f81
Alex Burlacu <alexandruburlacu03042000@gmail.com>
2024-02-04 22:42:12 +0100

    inserimento regola per far sommare i numeri e inserimento somma in pagina

--- Snack 2/script.js
+++ Snack 2/script.js
@@ -1,5 +1,8 @@
 let number;
+let sommaNumeri = 0;
 
 for(let i = 0; i < 10; i++) {
-    number = Number(prompt("Please insert number"));   
+    number = Number(prompt("Please insert number"));
+    sommaNumeri += number;
+    document.getElementById("somma-numeri").innerHTML = sommaNumeri;   
 }
